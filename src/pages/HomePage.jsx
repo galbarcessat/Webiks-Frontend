b47846d94992015dfc389348b112f0ca2a0b0bc3
galbarcessat <galbarcessat1@gmail.com>
2024-08-06 11:59:39 +0300

    changed title and favicon

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,12 @@
+import { Map } from "../cmps/Map";
+import { SearchStore } from "../cmps/SearchStore";
+
 export function HomePage() {
 
     return (
-        <div>Home page!</div>
+        <div className="home-page-container">
+            <Map />
+            <SearchStore />
+        </div>
     )
 }
