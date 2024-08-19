4b4ff5cc12c3ea515d3affc0030c1d3649e22b52
galbarcessat <galbarcessat1@gmail.com>
2024-08-19 20:28:13 +0300

    changed vite config so build will go to backend folder

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, '../Webiks Backend/public'),
+    emptyOutDir: true
+    }
 })
